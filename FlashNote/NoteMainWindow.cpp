b438fbd599c56882501da2e39cbb0414255f979a
legobadman <lance5158387@gmail.com>
2021-04-05 18:26:25 +0800

    添加ui文件及图标

--- FlashNote/NoteMainWindow.cpp
+++ FlashNote/NoteMainWindow.cpp
@@ -1,14 +1,24 @@
 #include "stdafx.h"
+#include "ui_NoteMainWindow.h"
 #include "NoteMainWindow.h"
 #include "moc_NoteMainWindow.cpp"
+
 
 NoteMainWindow::NoteMainWindow(QWidget* parent)
 	:QMainWindow(parent)
 {
-
+	init();
 }
 
 NoteMainWindow::~NoteMainWindow()
 {
+}
 
+void NoteMainWindow::init()
+{
+	m_ui = new Ui::NoteMainWindow();
+	m_ui->setupUi(this);
+
+	setWindowIcon(QIcon(":/icons/flash.png"));
+	
 }
--- FlashNote/NoteMainWindow.h
+++ FlashNote/NoteMainWindow.h
@@ -1,12 +1,22 @@
 #ifndef __NOTE_MAIN_WINDOW_H__
 #define __NOTE_MAIN_WINDOW_H__
 
+namespace Ui
+{
+	class NoteMainWindow;
+}
+
 class NoteMainWindow : public QMainWindow
 {
 	Q_OBJECT
 public:
 	NoteMainWindow(QWidget* parent);
 	~NoteMainWindow();
+
+	void init();
+
+private:
+	Ui::NoteMainWindow* m_ui;
 };
 
 #endif
