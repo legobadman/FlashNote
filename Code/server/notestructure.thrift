6ba238065b4199249879bfc7124b49bbb25c7a2c
legobadman <lance5158387@gmail.com>
2021-05-06 18:04:05 +0800

    修改thrift

--- Code/server/notestructure.thrift
+++ Code/server/notestructure.thrift
@@ -2,11 +2,20 @@ struct Note {
 	1:required string id;
 	2:required string title;
 	3:required string text_abbre; //ÎÄ±¾ËõÂÔ
+	4:required string notebook_id;
+	5:required string creater_id;
+	6:required list<string> owners;
+	7:required Timestamp create_time;
+	8:required Timestamp modify_time;
 }
 
 struct Notebook {
-	1:required list<Note> notes;
-	2:required string name;
+	1:required string id;
+	2:required list<Note> notes;
+	3:required string name;
+	4:required Timestamp create_time;
+	5:required Timestamp modify_time;
+	6:required string creater;
 }
 
 service UserServer {
